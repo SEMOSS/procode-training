c9670077e401b9a9932353c70934de0f4b037063
Van Buren <tevanburen@deloitte.com>
2025-06-24 16:43:00 -0400

    1+1

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,19 @@
+import { Env } from '@semoss/sdk';
+import { InsightProvider } from '@semoss/sdk-react';
+
+if (process.env.NODE_ENV !== 'production') {
+    Env.update({
+        MODULE: process.env.MODULE || '',
+        ACCESS_KEY: process.env.ACCESS_KEY || '',
+        SECRET_KEY: process.env.SECRET_KEY || '',
+        APP: process.env.APP || '',
+    });
+}
+
 export const App = () => {
-    return <div>Hello world</div>;
+    return (
+        <InsightProvider>
+            <div>Hello world</div>
+        </InsightProvider>
+    );
 };
