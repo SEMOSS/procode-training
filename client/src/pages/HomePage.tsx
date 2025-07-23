abc3a08c90ff8667fe5b3cf45346ba101cf17cf9
Van Buren <tevanburen@deloitte.com>
2025-07-23 14:47:24 -0400

    call llm

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,4 +1,6 @@
+import { Engine } from '@/components';
 import { useAppContext } from '@/contexts';
+import { useLoadingState } from '@/hooks';
 import {
     Autocomplete,
     Button,
@@ -7,6 +9,7 @@ import {
     TextField,
     Typography,
 } from '@mui/material';
+import { useState } from 'react';
 
 const StyledStack = styled(Stack)(({ theme }) => ({
     backgroundColor: theme.palette.background.paper,
@@ -19,7 +22,39 @@ const StyledStack = styled(Stack)(({ theme }) => ({
  * @component
  */
 export const HomePage = () => {
-    const { models, databases } = useAppContext();
+    const { models, databases, runPixel } = useAppContext();
+
+    /**
+     * State
+     */
+    const [model, setModel] = useState<Engine>(null);
+    const [db, setDb] = useState<Engine>(null);
+    const [prompt, setPrompt] = useState<string>('');
+    const [response, setResponse] = useState<string>('');
+    const [isResponseLoading, setIsResponseLoading] = useLoadingState(false);
+    const [error, setError] = useState<boolean>(false);
+
+    /**
+     * Functions
+     */
+    const submitPrompt = async () => {
+        const loadingKey = setIsResponseLoading(true);
+        try {
+            const response = await runPixel<string>(
+                `LLM2( engine = ${JSON.stringify(model.app_id)}, command = ${JSON.stringify(prompt)} )`,
+            );
+            setIsResponseLoading(false, loadingKey, () =>
+                setResponse(response),
+            );
+        } catch {
+            setIsResponseLoading(false, loadingKey, () => setError(true));
+        }
+    };
+
+    /**
+     * Constants
+     */
+    const isSubmitDisabled = !model?.app_id || !db?.app_id || !prompt.trim();
 
     return (
         <Stack width="100%" alignItems="center">
@@ -34,6 +69,8 @@ export const HomePage = () => {
                     </Typography>
 
                     <Autocomplete
+                        value={model}
+                        onChange={(_, val) => setModel(val)}
                         options={models}
                         getOptionKey={(model) => model.app_id}
                         getOptionLabel={(model) => model.app_name}
@@ -44,6 +81,8 @@ export const HomePage = () => {
                     />
 
                     <Autocomplete
+                        value={db}
+                        onChange={(_, val) => setDb(val)}
                         options={databases}
                         getOptionKey={(db) => db.app_id}
                         getOptionLabel={(db) => db.app_name}
@@ -59,17 +98,39 @@ export const HomePage = () => {
                         Prompt
                     </Typography>
 
-                    <TextField multiline rows={3} />
+                    <TextField
+                        multiline
+                        rows={3}
+                        value={prompt}
+                        onChange={(e) => setPrompt(e.target.value)}
+                    />
                 </Stack>
 
-                <Button variant="contained">Submit</Button>
+                <Button
+                    variant="contained"
+                    onClick={submitPrompt}
+                    loading={isResponseLoading}
+                    disabled={isSubmitDisabled}
+                >
+                    Submit
+                </Button>
 
                 <Stack spacing={1}>
                     <Typography variant="h6" fontWeight="bold">
                         Response
                     </Typography>
 
-                    <TextField multiline minRows={3} />
+                    <TextField
+                        multiline
+                        minRows={3}
+                        value={
+                            isResponseLoading
+                                ? 'Loading...'
+                                : error
+                                  ? 'An error occurred. Please try again later. '
+                                  : response
+                        }
+                    />
                 </Stack>
             </StyledStack>
         </Stack>
