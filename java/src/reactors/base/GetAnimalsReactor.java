b65261fbb6d6a737f5b75bfa5d6324d376ccec74
Van Buren <tevanburen@deloitte.com>
2025-07-23 09:31:16 -0400

    feat: start BE

---
+++ java/src/reactors/base/GetAnimalsReactor.java
@@ -0,0 +1,34 @@
+package reactors.base;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import prerna.sablecc2.om.PixelDataType;
+import prerna.sablecc2.om.nounmeta.NounMetadata;
+import reactors.AbstractTQMCReactor;
+import util.TQMCHelper;
+
+public class GetAnimalsReactor extends AbstractTQMCReactor {
+
+  @Override
+  protected NounMetadata doExecute(Connection con) throws SQLException {
+
+    // List<MTFData> output = new ArrayList<>();
+    // try (PreparedStatement mtfQuery =
+    //     con.prepareStatement(
+    //         "SELECT DMIS_ID, MTF_NAME, COALESCE(alias_mtf_name, mtf_name) AS ALIAS_MTF_NAME FROM MTF ORDER BY LOWER(COALESCE(alias_mtf_name, mtf_name)) ASC"); ) {
+    //   if (mtfQuery.execute()) {
+    //     ResultSet rs = mtfQuery.getResultSet();
+    //     while (rs.next()) {
+    //       String id = rs.getString("DMIS_ID");
+    //       String name = rs.getString("MTF_NAME");
+    //       String alias = rs.getString("ALIAS_MTF_NAME");
+    //       MTFData row = new MTFData(name, id, alias);
+    //       output.add(row);
+    //     }
+    //   }
+    // }
+    int output = TQMCHelper.addIntegerExampleHelper(con, 5, 7);
+
+    return new NounMetadata(output, PixelDataType.CONST_INT);
+  }
+}
--- java/src/util/TQMCHelper.java
+++ java/src/util/TQMCHelper.java
@@ -4,25 +4,20 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
-import domain.base.MTFData;
 
 public class TQMCHelper {
-  public static List<MTFData> getMtfs(Connection con) throws SQLException {
-    List<MTFData> output = new ArrayList<>();
-    try (PreparedStatement mtfQuery =
+  public static int addIntegerExampleHelper(Connection con, int a, int b) throws SQLException {
+    int output = 0;
+    try (PreparedStatement ps =
         con.prepareStatement(
-            "SELECT DMIS_ID, MTF_NAME, COALESCE(alias_mtf_name, mtf_name) AS ALIAS_MTF_NAME FROM MTF ORDER BY LOWER(COALESCE(alias_mtf_name, mtf_name)) ASC"); ) {
-      if (mtfQuery.execute()) {
-        ResultSet rs = mtfQuery.getResultSet();
+            "SELECT ? + ? AS OUTPUT"); ) {
+      int parameterIndex = 1;
+      ps.setInt(parameterIndex++, a);
+      ps.setInt(parameterIndex++, b);
+      if (ps.execute()) {
+        ResultSet rs = ps.getResultSet();
         while (rs.next()) {
-          String id = rs.getString("DMIS_ID");
-          String name = rs.getString("MTF_NAME");
-          String alias = rs.getString("ALIAS_MTF_NAME");
-          MTFData row = new MTFData(name, id, alias);
-          output.add(row);
+          output = rs.getInt("OUTPUT");
         }
       }
     }
