0af106693e4a9528d5cf90bd17b43e2eb990b277
Van Buren <tevanburen@deloitte.com>
2025-07-29 16:49:49 -0400

    ui on page

--- client/src/pages/AnimalPage.tsx
+++ client/src/pages/AnimalPage.tsx
@@ -1,5 +1,6 @@
 import { Animal, AnimalList } from '@/components';
 import { useLoadingPixel, useSettingPixel } from '@/hooks';
+import { Button, Stack, Typography } from '@mui/material';
 
 /**
  * Renders a page for the animal example.
@@ -7,22 +8,34 @@ import { useLoadingPixel, useSettingPixel } from '@/hooks';
  * @component
  */
 export const AnimalPage = () => {
-    const [animalList, isAnimalListLoading] = useLoadingPixel<Animal[]>(
-        'GetAnimals( )',
-        [],
-    );
-
+    /**
+     * State
+     */
+    const [animalList, isAnimalListLoading, fetchAnimalList] = useLoadingPixel<
+        Animal[]
+    >('GetAnimals( )', []);
     const [addAnimal] = useSettingPixel();
 
+    /**
+     * Functions
+     */
     const clickButton = async () => {
-        addAnimal('1 + 3', (response) => console.log(response));
+        addAnimal('1 + 3', () => fetchAnimalList());
     };
 
     return (
-        <div>
-            <div>Animal Page</div>
-            <button onClick={clickButton}>Button</button>
+        <Stack>
+            <Stack
+                direction="row"
+                alignItems="center"
+                justifyContent="space-between"
+            >
+                <Typography variant="h4">Animal Page</Typography>
+                <Button onClick={clickButton} variant="contained">
+                    Add animal
+                </Button>
+            </Stack>
             <AnimalList animalList={isAnimalListLoading ? [] : animalList} />
-        </div>
+        </Stack>
     );
 };
