d1a780df0fe643b4449bfa882a1605e4a7e41d7f
Van Buren <tevanburen@deloitte.com>
2025-09-17 16:41:16 -0400

    feat: enhance error

--- client/src/pages/ErrorPage.tsx
+++ client/src/pages/ErrorPage.tsx
@@ -1,9 +1,24 @@
+import { ErrorOutline } from '@mui/icons-material';
+import { Stack, Typography } from '@mui/material';
+
 /**
  * Renders a warning message for any FE errors encountered.
  *
  * @component
  */
 export const ErrorPage = () => {
-    console.log('ErrorPage rendered due to an error');
-    return <div>Error: Page not found</div>;
+    return (
+        <Stack
+            height="100%"
+            alignItems="center"
+            justifyContent="center"
+            spacing={1}
+        >
+            <ErrorOutline color="error" fontSize="large" />
+            <Typography variant="body1">
+                An error has occurred. Please try again or contact support if
+                the problem persists.
+            </Typography>
+        </Stack>
+    );
 };
