874fed6c0421359c9124eafbe4286ead57d9f8f7
Van Buren <tevanburen@deloitte.com>
2025-09-18 11:32:14 -0400

    fix: stringify

--- client/src/components/examples/DeleteAnimalModal.tsx
+++ client/src/components/examples/DeleteAnimalModal.tsx
@@ -28,8 +28,9 @@ export const DeleteAnimalModal = ({
      * Functions
      */
     const handleSubmitClick = async () => {
-        runPixel(`DeleteAnimal(${animalToDelete.animal_id})`, () =>
-            onClose(true),
+        runPixel(
+            `DeleteAnimal(${JSON.stringify(animalToDelete.animal_id)})`,
+            () => onClose(true),
         );
     };
 
