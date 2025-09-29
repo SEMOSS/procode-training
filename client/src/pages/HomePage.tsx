a9d4625f431a694ad02ae894ad8052285ae49298
Van Buren <tevanburen@deloitte.com>
2025-09-29 12:28:42 -0400

    feat: fetch models

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,4 +1,10 @@
-import { Stack, Typography } from '@mui/material';
+import { useLoadingPixel } from '@/hooks';
+import { Autocomplete, Stack, TextField, Typography } from '@mui/material';
+
+interface Engine {
+    app_id: string;
+    app_name: string;
+}
 
 /**
  * Renders the home page.
@@ -6,10 +12,33 @@ import { Stack, Typography } from '@mui/material';
  * @component
  */
 export const HomePage = () => {
+    /**
+     * Library hooks
+     */
+    const [engines, isLoadingEngines] = useLoadingPixel<Engine[]>(
+        `MyEngines( engineTypes=["MODEL"] )`,
+    );
+
+    console.log(engines, isLoadingEngines);
+
     return (
         <Stack spacing={2}>
             <Typography variant="h4">Home page</Typography>
             <Typography>Meeting minutes ...</Typography>
+            {isLoadingEngines ? (
+                <Typography>Loading engines...</Typography>
+            ) : (
+                <>
+                    <Autocomplete
+                        options={engines || []}
+                        getOptionLabel={(option) => option.app_name}
+                        renderInput={(params) => (
+                            <TextField {...params} label="Model" />
+                        )}
+                        getOptionKey={(option) => option.app_id}
+                    />
+                </>
+            )}
         </Stack>
     );
 };
