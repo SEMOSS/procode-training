24e7d5ee9779adf0cee31278cd61e8599eba09b8
Van Buren <tevanburen@deloitte.com>
2025-09-29 11:30:50 -0400

    fix: unsaved change

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,4 @@
-import path from 'path';
+import { resolve } from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -11,7 +11,7 @@ export default defineConfig(({ mode }) => {
         envPrefix: 'CLIENT_',
         resolve: {
             alias: {
-                '@': path.resolve(__dirname, './src'),
+                '@': resolve(__dirname, './src'),
             },
         },
         server: {
