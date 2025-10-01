c8f0e46057b5845575c4682894c6a54db2fe0394
Van Buren <tevanburen@deloitte.com>
2025-10-01 10:52:36 -0400

    feat: add files

--- client/src/components/VectorFiles.tsx
+++ client/src/components/VectorFiles.tsx
@@ -1,6 +1,7 @@
-import { useLoadingPixel } from '@/hooks';
+import { useLoadingPixel, useLoadingState, useSettingPixel } from '@/hooks';
 import { Stack, Typography } from '@mui/material';
 import { Dropzone } from './library';
+import { useInsight } from '@semoss/sdk-react';
 
 export interface VectorFilesProps {
     engineId?: string;
@@ -21,12 +22,39 @@ export const VectorFiles = ({ engineId }: VectorFilesProps) => {
     /**
      * Library hooks
      */
-    const [files, isLoadingFiles] = useLoadingPixel<SemossFile[]>(
+    const [files, isLoadingFiles, loadFiles] = useLoadingPixel<SemossFile[]>(
         engineId
             ? `ListDocumentsInVectorDatabase(engine=${JSON.stringify(engineId)})`
             : undefined,
         [],
     );
+    const [runAddDocsPixel] = useSettingPixel();
+    const { actions } = useInsight();
+    const [isUploadingDocuments, setIsUploadingDocuments] = useLoadingState();
+
+    /**
+     * Functions
+     */
+    const handleNewFiles = async (newFiles: File[]) => {
+        const loadingKey = setIsUploadingDocuments(true);
+        const afterUpload = () => {
+            setIsUploadingDocuments(false, loadingKey, loadFiles);
+        };
+
+        try {
+            const uploadedFiles = await actions.upload(newFiles, '');
+            const filePaths = uploadedFiles.map((file) =>
+                file.fileLocation.slice(1),
+            ); // Remove leading '/'
+            runAddDocsPixel(
+                `CreateEmbeddingsFromDocuments (engine = ${JSON.stringify(engineId)}, filePaths = ${JSON.stringify(filePaths)});`,
+                afterUpload,
+                afterUpload,
+            );
+        } catch {
+            afterUpload();
+        }
+    };
 
     return !engineId || isLoadingFiles ? (
         'Loading...'
@@ -34,9 +62,8 @@ export const VectorFiles = ({ engineId }: VectorFilesProps) => {
         <Stack>
             <Typography>Files in Vector Database:</Typography>
             <Dropzone
-                handleNewFiles={(files) =>
-                    console.log('New files added', files)
-                }
+                handleNewFiles={handleNewFiles}
+                disabled={isUploadingDocuments}
             />
             {files.map((file) => (
                 <Stack
